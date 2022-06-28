520827bde71694bb5f8beebd67e4b8f086f7efac
kuldeep H M <kumanpatel51@gmail.com>
2022-06-28 07:23:36 +0530

    shell scripting added

---
+++ file3.sh
@@ -0,0 +1,5 @@
+echo "enter first number"
+read number1
+echo "enter second number"
+read number2
+echo "sum is"$((number1 + number2))
---
+++ file5.sh
@@ -0,0 +1,6 @@
+echo $1
+echo $2
+echo  "sum is"$(($1+$2))
+echo " sum is"`expr $1 + $2`
+let sum=$1+$2
+echo "sum is" $sum
